501ac3029479966850cc27af182f3352de6bc30f
Arthur <67799784+arthuraraujo2305@users.noreply.github.com>
2025-03-13 09:57:57 -0300

    Add files via upload

---
+++ Aula02/Universidade_Comosicao/src/main/java/com/mycompany/universidade_comosicao/Aluno.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.universidade_comosicao;
+
+/**
+ *
+ * @author 14651458
+ */
+public class Aluno {
+    private String nome;
+    private int nusp;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNusp() {
+        return nusp;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNusp(int nusp) {
+        this.nusp = nusp;
+    }
+
+    public Aluno(String nome, int nusp) {
+        this.nome = nome;
+        this.nusp = nusp;
+    }
+}
---
+++ Aula02/Universidade_Comosicao/src/main/java/com/mycompany/universidade_comosicao/Departamento.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.universidade_comosicao;
+
+/**
+ *
+ * @author 14651458
+ */
+public class Departamento {
+    private String nome;
+    private int cod;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public Departamento(String nome) {
+        this.nome = nome;
+    }
+    
+}
